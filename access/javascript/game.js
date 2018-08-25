f5793137c56f1603d07fbae968789e4f877406b0
The Singer <kayla312@harbornet.com>
2018-08-25 01:49:16 -0700

    added in some of the if conditions with variable decleration. added alerts when they answer right and wrong. added notes to tell what each piece of the code does what

--- access/javascript/game.js
+++ access/javascript/game.js
@@ -1,6 +1,8 @@
 // creating variable to hol the number of wins and losses as the computer and user interact
 var wins = 0;
 var losses= 0;
+var guessesLeft= 10;
+var guessedLetters= [];
 
 // determines which key was pressed by the user
 var userGuess = event.key;
@@ -15,7 +17,16 @@ var randomLetterList = ["a", "b", "c", "d", "e",
  var randomLetter = randomLetterList[Math.floor(math.random() * randomLetterList.length)];
 if (userGuess === randomLetter) {
     wins++
+    guessesLeft = 10;
+    guessedLetters = [];
 }
-else 
-
- var guessedLetters = [ userGuess];
+if (userGuess != randomLetter){
+    guessesLeft -1;
+    guessedletters.push(userGuess);
+}
+if (guessesLeft === 0){
+    guessesLeft = 10;
+    losses ++;
+    guessedLetters = [];
+}
+//  var guessedLetters = [userGuess];
