d0acd59b7d142422ecc66393e73d556f176018cc
The Singer <kayla312@harbornet.com>
2018-08-25 03:00:11 -0700

    making some easthetic changes, because i cannot get my code to link and change the win/loss/link in list and what not

--- access/javascript/game.js
+++ access/javascript/game.js
@@ -2,31 +2,51 @@
 var wins = 0;
 var losses= 0;
 var guessesLeft= 10;
+var computerLetter=[];
 var guessedLetters= [];
-
-// determines which key was pressed by the user
-var userGuess = event.key;
-
 
 // list of possible letters for the computer to choose from as an unknown letter
 var randomLetterList = ["a", "b", "c", "d", "e",
  "f", "g", "h", "i", "j", "k", "l",
   "m", "n", "o", "p", "q", "r", "s",
  "t", "u", "v", "w", "x", "y", "z"];
-//  choose a random letter from the array above
- var randomLetter = randomLetterList[Math.floor(math.random() * randomLetterList.length)];
+//  computer choose a random letter from the array above
+ var randomLetter = randomLetterList[Math.floor(Math.random() * randomLetterList.length)];
+    computerLetter.push(randomLetter);
+
+// determines which key was pressed by the user
+document.onkeyup = function(event){
+var userGuess = event.key;
+guessedLetters.push(userGuess);
+
+//  if the users typed guess is the same as the computers code letter, the number of wons goes up
 if (userGuess === randomLetter) {
+    alert("Nicely done! Thats the letter I was thinking of!")
     wins++
     guessesLeft = 10;
     guessedLetters = [];
 }
-if (userGuess != randomLetter){
+// if the users guess doesnt match the computers code letter, then the number of guesses goes down by one, the letter is put into the brackets and
+else if (userGuess != randomLetter){
+    alert("nope! choose again")
     guessesLeft -1;
     guessedletters.push(userGuess);
 }
-if (guessesLeft === 0){
+if (guessesLeft === 0) {
     guessesLeft = 10;
     losses ++;
     guessedLetters = [];
 }
+// attaching the numbers to the lines of text that identify them
+var html = 
+"<h2> Guess what letter I'm thinking of !!</h2>" +
+"<div>Wins:" + wins + "</div>" +
+"<div>Losses:" + losses + "</div>" +
+"<div>Guesses Left:" + guessesLeft + "</div>" +
+"<div>Guesses Made:" + guessesLetters + "</div>";
+
+document.querySelector('#game').innerHTML = html;
+
+
 //  var guessedLetters = [userGuess];
+}
